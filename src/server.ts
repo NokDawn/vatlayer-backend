c9965024e355f3e30b946eace0dbe706b19ebefe
Mateusz Wojtas <nookdawn@gmail.com>
2023-03-25 22:12:10 +0100

    feat: Add custom error class

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,8 @@
 import {
+  CustomError,
+  IErrorResponse,
+} from './shared/globals/helpers/error-handler';
+import {
   Application,
   json,
   urlencoded,
@@ -65,7 +69,29 @@ export class VatLayerServer {
     applicationRoutes(app);
   }
 
-  private globalErrorHandler(app: Application): void {}
+  private globalErrorHandler(app: Application): void {
+    app.all('*', (req: Request, res: Response) => {
+      res.status(HTTP_STATUS.NOT_FOUND).json({
+        status: 'error',
+        message: `${req.originalUrl} not found.`,
+      });
+    });
+
+    app.use(
+      (
+        error: IErrorResponse,
+        _req: Request,
+        res: Response,
+        next: NextFunction
+      ) => {
+        console.log(error);
+        if (error instanceof CustomError) {
+          return res.status(error.statusCode).json(error.serializeErrors());
+        }
+        next();
+      }
+    );
+  }
 
   private async startServer(app: Application): Promise<void> {
     try {
---
+++ src/shared/globals/helpers/error-handler.ts
@@ -0,0 +1,76 @@
+import HTTP_STATUS from 'http-status-codes';
+
+export interface IErrorResponse {
+  message: string;
+  statusCode: number;
+  status: string;
+  serializeErrors(): IError;
+}
+
+export interface IError {
+  message: string;
+  statusCode: number;
+  status: string;
+}
+
+export abstract class CustomError extends Error {
+  abstract statusCode: number;
+  abstract status: string;
+
+  constructor(message: string) {
+    super(message);
+  }
+
+  serializeErrors(): IError {
+    return {
+      message: this.message,
+      statusCode: this.statusCode,
+      status: this.status,
+    };
+  }
+}
+
+export class BadRequestError extends CustomError {
+  statusCode = HTTP_STATUS.BAD_REQUEST;
+  status = 'error';
+
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export class UnauthorizedError extends CustomError {
+  statusCode = HTTP_STATUS.UNAUTHORIZED;
+  status = 'error';
+
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export class ForbiddenError extends CustomError {
+  statusCode = HTTP_STATUS.FORBIDDEN;
+  status = 'error';
+
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export class NotFoundError extends CustomError {
+  statusCode = HTTP_STATUS.NOT_FOUND;
+  status = 'error';
+
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export class InternalServerError extends CustomError {
+  statusCode = HTTP_STATUS.INTERNAL_SERVER_ERROR;
+  status = 'error';
+
+  constructor(message: string) {
+    super(message);
+  }
+}
