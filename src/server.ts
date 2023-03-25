a732ed86cbf540b4f830b826682d0ca41cbbf72c
Mateusz Wojtas <nookdawn@gmail.com>
2023-03-25 21:40:35 +0100

    feat: Add security and standard middlewares

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,13 @@ import {
   NextFunction,
 } from 'express';
 import http from 'http';
+import cors from 'cors';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import cookieSession from 'cookie-session';
+import compression from 'compression';
+import HTTP_STATUS from 'http-status-codes';
+import 'express-async-errors';
 
 export class VatLayerServer {
   private app: Application;
@@ -23,9 +30,32 @@ export class VatLayerServer {
     this.startServer(this.app);
   }
 
-  private securityMiddleware(app: Application): void {}
+  private securityMiddleware(app: Application): void {
+    app.use(
+      cookieSession({
+        name: 'session',
+        keys: ['test1', 'test2'],
+        maxAge: 24 * 60 * 60 * 1000, // 24 hours
+        secure: false, // change to true if https
+      })
+    );
+    app.use(hpp());
+    app.use(helmet());
+    app.use(
+      cors({
+        origin: '*', // change to url after deploy
+        credentials: true,
+        optionsSuccessStatus: 200,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      })
+    );
+  }
 
-  private standardMiddleware(app: Application): void {}
+  private standardMiddleware(app: Application): void {
+    app.use(compression());
+    app.use(json({ limit: '50mb' }));
+    app.use(urlencoded({ extended: true, limit: '50mb' }));
+  }
 
   private routeMiddleware(app: Application): void {}
 
